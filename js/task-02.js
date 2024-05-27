106bcff913370fdc02608cfb4eabbdb846491cfa
Sergiybas <163581263+Sergiybas@users.noreply.github.com>
2024-05-27 20:50:46 +0300

    DOM123

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,51 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// const titlePotatoes = document.createElement("li");
+// titlePotatoes.classList.add("item");
+// titlePotatoes.textContent = ingredients[0];
+// const titleMushrooms = document.createElement("li");
+// titleMushrooms.classList.add("item");
+// titleMushrooms.textContent = ingredients[1];
+// const titleGarlic = document.createElement("li");
+// titleGarlic.classList.add("item");
+// titleGarlic.textContent = ingredients[2];
+// const titleTomatos = document.createElement("li");
+// titleTomatos.classList.add("item");
+// titleTomatos.textContent = ingredients[3];
+// const titleHerbs = document.createElement("li");
+// titleHerbs.classList.add("item");
+// titleHerbs.textContent = ingredients[4];
+// const titleCondiments = document.createElement("li");
+// titleCondiments.classList.add("item");
+// titleCondiments.textContent = ingredients[5];
+// const itemUi = document.querySelector('#ingredients')
+// itemUi.append(
+//   titlePotatoes,
+//   titleMushrooms,
+//   titleGarlic,
+//   titleTomatos,
+//   titleHerbs,
+//   titleCondiments
+// );
+// console.log(itemUi);
+//////////////////////////////////////////////////////
+//////////////////variant2/////////////////////////////
+const title = document.querySelector("#ingredients");
+function ingredientsAdd(arr) {
+  const ingredientsFor = arr.map((ar) => {
+    const createLi = document.createElement("li");
+    createLi.classList.add("item");
+    createLi.textContent = ar;
+    return createLi;
+  });
+  title.append(...ingredientsFor);
+}
+
+ingredientsAdd(ingredients);
+console.log(title);
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,46 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+// const list = document.querySelector(".gallery");
+// function createImage(arr) {
+//   const imagesMap = arr.map(({ url, alt }) => {
+//     const imagesLi = document.createElement("li");
+//     imagesLi.style.marginBottom = "80px";
+//     const imagesImg = document.createElement("img");
+//     imagesImg.style.width = "400px";
+//     imagesImg.src = url;
+//     imagesImg.alt = alt;
+//     imagesLi.append(imagesImg);
+//     return imagesLi;
+//   });
+//   list.append(...imagesMap);
+// }
+// createImage(images);
+// console.log(list);
+///////////////////////////////////////////////////////////
+//////////////////variant2//////////////////////////////
+const list = document.querySelector(".gallery");
+function createImage(arr) {
+  const imagesMap = arr
+    .map(
+      ({ url, alt }) =>
+        ` <li>
+  <img src="${url}" alt="${alt}" width = '600'>
+   </li>`
+    )
+    .join("");
+
+  list.insertAdjacentHTML("beforebegin", imagesMap);
+}
+createImage(images);
